2b5866e331d5b1aac5ce068587939a610ef848ac
btssio <btssio@btssio-VirtualBox>
2017-03-08 17:25:18 +0100

    testActiviteCompl et début DaoLabo

--- src/test/modele/metier/testActiviteCompl.java
+++ src/test/modele/metier/testActiviteCompl.java
@@ -5,7 +5,9 @@
  */
 package test.modele.metier;
 
+import java.util.Calendar;
 import modele.metier.ActiviteCompl;
+import java.util.Date;
 /**
  *
  * @author btssio
@@ -16,20 +18,21 @@ public class testActiviteCompl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        ActiviteCompl ActC, adr1, adr2;
+        Date dateAct = new Date(2017-11-1);
+       
+        ActiviteCompl ActC, ActC1, ActC2;
         System.out.println("\nTest n°1 : instanciation et accesseurs");
-        ActC = new ActiviteCompl(1, ,  );
-        System.out.println(adr);
-        System.out.println("\nTest n°2 : mutateurs");
-        ActC.setRue("56 boulevard de la Prairie aux Ducs");
-        ActC.setCp("44265");
-        ActC.setVille("Nantes");
+        ActC = new ActiviteCompl(1,dateAct, "lieu", "theme", "motif");
         System.out.println(ActC);
-        adr1 = new ActiviteCompl(1,null, null, null);
-        System.out.println(adr1.equals(adr));
-        adr2 = new ActiviteCompl(2,null, null, null);
-        System.out.println(adr1.equals(adr2));
+        System.out.println("\nTest n°2 : mutateurs");
+        ActC.setAc_lieu("56 boulevard de la Prairie aux Ducs");
+        ActC.setAc_motif("motif");
+        ActC.setAc_theme("le theme ");
+        System.out.println(ActC);
+        ActC1 = new ActiviteCompl(1,null, null, null, "motif");
+        System.out.println(ActC1.equals(ActC));
+        ActC2 = new ActiviteCompl(2,null, null, null, "motif");
+        System.out.println(ActC1.equals(ActC2));
     
     }
     
